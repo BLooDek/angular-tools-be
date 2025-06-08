08c3a5b7ce86e4c7069b041d7147bea186e39bed
Pawel Michalak <pawelmichalak98@gmail.com>
2025-06-08 14:46:14 +0200

    set up prisma with mongodb

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,39 @@ import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import { swaggerOptions } from './config/swagger.config.js';
+import { PrismaClient } from './generated/prisma/client.js';
 
 export const port = process.env.APP_PORT || 3000;
 const app = express();
+const prisma = new PrismaClient();
 app.use(helmet());
+app.use(express.json());
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.post('/users', async (req, res) => {
+  try {
+    const { email, name } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error: { message: string } | any) {
+    res
+      .status(500)
+      .json({ error: 'Could not create user', details: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server on port http://localhost:${port}`);
   console.log(`Swagger UI available at http://localhost:${port}/api-docs`);
 });
+
+/*
+npx prisma generate
+*/
